875a30712a4d45009fcaa695e80dd15d52b2dcef
Lucas-Leocadio-dev <lucasleocadio05@gmail.com>
2024-05-03 22:28:18 -0300

    Criar conexão com a API e modificar inputbox da cidade

--- QueClima/index.js
+++ QueClima/index.js
@@ -1,29 +1,60 @@
-async function realizarRequisicao() {
-    // Arrow function pro evento de esconder botão e carregar loading
-    const buttonAnimation = () => {
-        var buttonSearch = document.getElementById("button-search");
-        var loading = document.getElementById("loading");
-        // Desativa o botão assim que clicado
-        buttonSearch.disabled = true;
-        // Escondendo o botão
-        buttonSearch.style.opacity = 0;
+const apiKey = "2624348629b1d5ce175e2838060b49e6";
+const buttonSearch = document.getElementById("button-search");
+const cityInput = document.getElementById("city-input")
 
+// Arrow function pro evento de esconder botão e carregar loading
 
-    };
-    buttonAnimation();
-    const nomeCidade = document.getElementById("city-input").value.toLowerCase();
-    console.log(nomeCidade);
-    try {
-        const requisicao = await fetch(
-            `http://api.openweathermap.org/geo/1.0/direct?q=${nomeCidade}&limit=1&appid=2624348629b1d5ce175e2838060b49e6`
-        );
-        if (!requisicao.ok) {
-            throw new Error("Não foi possível achar o nome dessa cidade");
-        }
-        const data = await requisicao.json();
-
-        console.log(data.name);
-    } catch (error) {
-        console.log(error);
-    }
+function pesquisarLocal() {
+    var loading = document.getElementById("loading");
+    // Desativa o botão assim que clicado
+    buttonSearch.disabled = true;
+    // Escondendo o botão
+    buttonSearch.style.display = "none";
+    // Adicionando classe que possui evento fade-in dentro da div loading - div vai aparecer
+    loading.classList.add("fade-in");
+    realizarRequisicao();
 }
+
+// async function realizarRequisicao() {
+//   const nomeCidade = document.getElementById("city-input").value.toLowerCase();
+//   let vento, chanceChuva, temperatura, local, requisicao, informacao, lat, lon;
+//   console.log(nomeCidade);
+//   try {
+//     requisicao = await fetch(
+//       `http://api.openweathermap.org/data/2.5/weather?q=${nomeCidade}&units=metric&appid=${apiKey}`
+//     );
+//     if (!requisicao.ok) {
+//       throw new Error("Não foi possível achar o nome dessa cidade");
+//     }
+//     informacao = await requisicao.json();
+//     lon = informacao[0].lon;
+//     lat = informacao[0].lat;
+//     console.log(informacao[0]);
+//     requisicao = await fetch(
+//       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+//     );
+//     informacao = await requisicao.json();
+//     console.log(informacao);
+//     temperatura = informacao.main.temp - 273;
+//     console.log(temperatura);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+const pegarClima = async (city) => {
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(weatherUrl);
+    const data = await res.json();
+
+    console.log(data);
+    return data;
+}
+
+buttonSearch.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+    pegarClima(city);
+})
